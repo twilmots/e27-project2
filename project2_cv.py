40a9f0c6adbbadcfed13f042cc5a6437fd909e85
Tom Wilmots <twilmots@gmail.com>
2017-02-23 17:35:13 -0500

    changed a tiny thing

--- project2_cv.py
+++ project2_cv.py
@@ -346,12 +346,12 @@ def hybrid(imageA = 'Einstein2.png', imageB = 'jolie.png'):
 	imageB = imageB.astype(np.float32)
 
 	# Setting our parameters
-	sigmaA = 25
-	sigmaB = 10
+	sigmaA = 10
+	sigmaB = 5
 	kA = 1
 	kB = 1
 
-	kernel_size_A = (35,35)
+	kernel_size_A = (39,39)
 	kernel_size_B = (5,5)
 
 	# lopass filter
@@ -404,12 +404,4 @@ if __name__ == "__main__":
 	hybrid = hybrid()
 	labelAndWaitForKey(hybrid, 'Hybrid Image')
 	cv2.imwrite('HybridImage.png', hybrid)
-
-
-
-
-
-
-
-
 
